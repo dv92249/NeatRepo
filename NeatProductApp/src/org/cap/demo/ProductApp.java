78fab96ae204941d08cc52351cbe81ec1b2a3e8c
Training <Training@DIN39000541.corp.capgemini.com>
2016-07-07 10:29:10 +0530

    secondFixCase

--- NeatProductApp/src/org/cap/demo/ProductApp.java
+++ NeatProductApp/src/org/cap/demo/ProductApp.java
@@ -5,5 +5,6 @@ public class ProductApp {
 	public void productDisplay() {
 		System.out.println("Pdt display method");
 		System.out.println("Jaba endhokeyo happened");
+		System.out.println("secondFixCase");
 	}
 }
