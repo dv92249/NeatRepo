c2d91b37ea2957ea0573ff959233c4ab72e69fe9
Training <Training@DIN39000541.corp.capgemini.com>
2016-07-07 10:28:27 +0530

    Fix case

--- NeatProductApp/src/org/cap/demo/ProductApp.java
+++ NeatProductApp/src/org/cap/demo/ProductApp.java
@@ -4,6 +4,7 @@ public class ProductApp {
 
 	public void productDisplay() {
 		System.out.println("Pdt display method");
+		System.out.println("For fix case");
 		System.out.println("Jaba endhokeyo happened");
 	}
 }
