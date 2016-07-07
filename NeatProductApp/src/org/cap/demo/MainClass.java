d690d87a3bdabe1e9be38d68df9f2e2244126f09
Training <Training@DIN39000541.corp.capgemini.com>
2016-07-07 10:01:00 +0530

    Initial Commit

---
+++ NeatProductApp/src/org/cap/demo/MainClass.java
@@ -0,0 +1,16 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		ProductApp productApp = new ProductApp();
+		productApp.productDisplay();
+		greetings();
+	}
+
+	public static void greetings() {
+		System.out.println("Happy to see all pdt details");
+	}
+
+}
---
+++ NeatProductApp/src/org/cap/demo/ProductApp.java
@@ -0,0 +1,8 @@
+package org.cap.demo;
+
+public class ProductApp {
+
+	public void productDisplay() {
+		System.out.println("Pdt display method");
+	}
+}
